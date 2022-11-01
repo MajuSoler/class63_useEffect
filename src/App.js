b2bea127056328218073c85dcd1abb584def7780
majusoler <juliasolernunes@ymail.com>
2022-11-01 18:57:23 +0100

    useEffect in action

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [drinksList, setDrinksList] = useState([]);
+  //API =>  https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka
+
+  const callData = async () => {
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka"
+    );
+    setDrinksList(response.data.drinks);
+    // return data;
+  };
+
+  useEffect(() => {
+    callData();
+  }, []);
+
+  console.log(drinksList, "this is data");
+
+  const addOne = () => {
+    setNumber(number + 1);
+    console.log("Here", number);
+  };
+
+  useEffect(() => {
+    console.log("I am useEffect!", number);
+  }, [number]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my App
+      <button onClick={addOne}>Click me!</button>
+      <div>
+        {drinksList.map((drink) => {
+          return <p key={drink.idDrink}>{drink.strDrink}</p>;
+        })}
+      </div>
     </div>
   );
 }
